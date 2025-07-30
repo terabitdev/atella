9dda4c65207064c046791cc3cc3350e323df5e2b
mdkhan707 <mdaniyalkhan783@gmail.com>
2025-07-30 12:21:21 +0500

    favorite screen done

--- lib/Modules/Home/View/Screens/favourite_screen.dart
+++ lib/Modules/Home/View/Screens/favourite_screen.dart
@@ -1,4 +1,10 @@
+import 'package:atella/Modules/Home/Controllers/home_controller.dart';
+import 'package:atella/Modules/Home/View/Widgets/search_widget.dart';
+import 'package:atella/Widgets/custom_roundbutton.dart';
+import 'package:atella/core/themes/app_fonts.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
 
 class FavouriteScreen extends StatefulWidget {
   const FavouriteScreen({super.key});
@@ -8,11 +14,171 @@ class FavouriteScreen extends StatefulWidget {
 }
 
 class _FavouriteScreenState extends State<FavouriteScreen> {
+  // Static data for My Designs
+  final List<Map<String, String>> myDesigns = [
+    {
+      'image': 'assets/images/grid1.png',
+      'name': 'Project 1',
+      'date': '13 July 2025',
+    },
+    {
+      'image': 'assets/images/grid2.png',
+      'name': 'Project 2',
+      'date': '20Mar 2025',
+    },
+    {
+      'image': 'assets/images/grid2.png',
+      'name': 'Project 2',
+      'date': '20Mar 2025',
+    },
+    {
+      'image': 'assets/images/grid2.png',
+      'name': 'Project 2',
+      'date': '20Mar 2025',
+    },
+    {
+      'image': 'assets/images/grid2.png',
+      'name': 'Project 2',
+      'date': '20Mar 2025',
+    },
+    {
+      'image': 'assets/images/grid2.png',
+      'name': 'Project 2',
+      'date': '20Mar 2025',
+    },
+  ];
+  final controller = Get.put(HomeController());
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Favourite Screen'),
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 54.h),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  InkWell(
+                    onTap: () => Get.back(),
+                    child: Image.asset(
+                      'assets/images/Arrow_Left.png',
+                      height: 40.h,
+                      width: 40.w,
+                    ),
+                  ),
+                  SizedBox(width: 8.w),
+                  Text(
+                    'Favorites',
+                    style: SSTitleTextTextStyle208001.copyWith(
+                      color: Colors.black,
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 34.h),
+              SearchWidget(
+                controller: controller.searchController,
+                onChanged: controller.onSearchChanged,
+              ),
+              SizedBox(height: 20.h),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 16.w),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    GridView.builder(
+                      shrinkWrap: true,
+                      physics: NeverScrollableScrollPhysics(),
+                      itemCount: myDesigns.length,
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2,
+                        crossAxisSpacing: 16.w,
+                        mainAxisSpacing: 16.h,
+                        childAspectRatio: 0.6,
+                      ),
+                      itemBuilder: (context, index) {
+                        final project = myDesigns[index];
+                        return Container(
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(16),
+                          ),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              // Image with Heart Icon Overlay
+                              Stack(
+                                children: [
+                                  ClipRRect(
+                                    borderRadius: BorderRadius.vertical(top: Radius.circular(16)),
+                                    child: Image.asset(
+                                      project['image']!,
+                                      height: 177.h,
+                                      width: 162.w,
+                                      fit: BoxFit.cover,
+                                    ),
+                                  ),
+                                  // Heart Icon positioned at top-left
+                                  Positioned(
+                                    top: 8.h,
+                                    left: 8.w,
+                                    child: GestureDetector(
+                                      onTap: () {
+                                        // Handle favorite toggle
+                                        print('Favorite tapped for ${project['name']}');
+                                      },
+                                      child: Container(
+                                        padding: EdgeInsets.all(4.w),
+                                        child: Icon(
+                                          Icons.favorite,
+                                          color: Colors.black,
+                                          size: 20.sp,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              Padding(
+                                padding: EdgeInsets.all(12.w),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Row(
+                                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        Text(
+                                          project['name']!,
+                                          style: SSTitleTextTextStyle186004,
+                                        ),
+                                        Image.asset('assets/images/pen.png', height: 15.h, width: 15.w),
+                                      ],
+                                    ),
+                                    SizedBox(height: 6.h),
+                                    Text(
+                                      project['date']!,
+                                      style: TextStyle(
+                                        color: Colors.grey,
+                                        fontSize: 10.sp,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        );
+                      },
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 20.h),
+              RoundButton(title: 'Create New Design', onTap: () {}, color: Colors.black, isloading: false)
+            ],
+          ),
+        ),
       ),
     );
   }
